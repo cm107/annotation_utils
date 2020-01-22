21e073151c72f588873971e5b8f73a5dea47c633
Clayton Mork <mork.clayton3@gmail.com>
2020-01-22 11:27:50 +0900

    Refactored COCOAnnotationVisualizer to support keypoint and bbox labels.

--- build/lib/annotation_utils/coco/annotation/visualizer/coco_annotation_visualizer.py
+++ build/lib/annotation_utils/coco/annotation/visualizer/coco_annotation_visualizer.py
@@ -90,6 +90,8 @@ class COCOAnnotationVisualizer:
         bbox_color: list=[0, 255, 255], kpt_color: list=[0, 0, 255], skeleton_color: list=[255, 0, 0], seg_color: list=[255, 255, 0],
         transparent_seg: bool=True, kpt_radius: int=10, bbox_thickness: int=2, skeleton_thickness: int=2, show_cat_name: bool=False,
         skeleton_index_offset: int=0,
+        show_bbox: bool=True, show_kpts: bool=True, show_skeleton: bool=True, show_seg: bool=True,
+        show_bbox_labels: bool=False, show_kpt_labels: bool=False, bbox_label_thickness: int=None, kpt_label_thickness: int=None,
         viz_limit: int=None
     ):
         self.img_dir = img_dir
@@ -112,10 +114,26 @@ class COCOAnnotationVisualizer:
         self.show_cat_name = show_cat_name
         self.skeleton_index_offset = skeleton_index_offset
 
+        self.show_bbox = show_bbox
+        self.show_kpts = show_kpts
+        self.show_skeleton = show_skeleton
+        self.show_seg = show_seg
+
+        self.show_bbox_labels = show_bbox_labels
+        self.show_kpt_labels = show_kpt_labels
+        self.bbox_label_thickness = bbox_label_thickness
+        self.kpt_label_thickness = kpt_label_thickness
+
         self.viz_limit = viz_limit
         self.viz_count = 0
 
-    def save(self, img: np.ndarray, bbox_list: list, kpt_list_list: list, kpt_skeleton_list: list, seg_list: list, cat_name_list: list, file_name: str):
+    def save(
+        self, img: np.ndarray,
+        bbox_list: list,
+        kpt_list_list: list, kpt_skeleton_list: list, kpt_label_list_list: list,
+        seg_list: list, cat_name_list: list,
+        file_name: str
+    ):
         result = img.copy()
         save_path = f"{self.visualization_dump_dir}/{file_name}"
         retry_count = 0
@@ -128,14 +146,31 @@ class COCOAnnotationVisualizer:
                 raise Exception
             retry_count += 1
 
-        for bbox, kpt_list, kpt_skeleton, seg, cat_name in zip(bbox_list, kpt_list_list, kpt_skeleton_list, seg_list, cat_name_list):
-            result = draw_segmentation(img=result, segmentation=seg, color=self.seg_color, transparent=self.transparent_seg)
-            result = draw_skeleton(
-                img=result, keypoints=kpt_list, keypoint_skeleton=kpt_skeleton, index_offset=self.skeleton_index_offset,
-                thickness=self.skeleton_thickness, color=self.skeleton_color
-            )
-            result = draw_keypoints(img=result, keypoints=kpt_list, radius=self.kpt_radius, color=self.kpt_color)
-            result = draw_bbox(img=result, bbox=bbox, color=self.bbox_color, thickness=self.bbox_thickness, text=cat_name)
+        for bbox, kpt_list, kpt_skeleton, kpt_label_list, seg, cat_name in \
+            zip(bbox_list, kpt_list_list, kpt_skeleton_list, kpt_label_list_list, seg_list, cat_name_list):
+            if self.show_seg:
+                result = draw_segmentation(img=result, segmentation=seg, color=self.seg_color, transparent=self.transparent_seg)
+            if self.show_skeleton:
+                result = draw_skeleton(
+                    img=result, keypoints=kpt_list, keypoint_skeleton=kpt_skeleton, index_offset=self.skeleton_index_offset,
+                    thickness=self.skeleton_thickness, color=self.skeleton_color
+                )
+            if self.show_kpts:
+                if self.kpt_label_thickness is not None:
+                    kpt_label_thickness = self.kpt_label_thickness
+                else:
+                    kpt_label_thickness = self.kpt_radius - 1 if self.kpt_radius > 1 else 1
+                result = draw_keypoints(
+                    img=result, keypoints=kpt_list, radius=self.kpt_radius, color=self.kpt_color, keypoint_labels=kpt_label_list,
+                    show_keypoints_labels=self.show_kpt_labels, label_thickness=kpt_label_thickness
+                )
+            if self.show_bbox:
+                bbox_text = cat_name if self.show_bbox_labels else None
+                bbox_label_thickness = self.bbox_label_thickness if self.bbox_label_thickness is not None else self.bbox_thickness
+                result = draw_bbox(
+                    img=result, bbox=bbox, color=self.bbox_color, thickness=self.bbox_thickness,
+                    text=bbox_text, label_thickness=bbox_label_thickness
+                )
         cv2.imwrite(filename=save_path, img=result)
         logger.info(f"Wrote {save_path}")
         self.viz_count += 1
@@ -151,6 +186,7 @@ class COCOAnnotationVisualizer:
             bbox_list = []
             kpt_list_list = []
             kpt_skeleton_list = []
+            kpt_label_list_list = []
             seg_list = []
             cat_name_list = []
             for coco_annotation in coco_annotation_list:
@@ -165,11 +201,13 @@ class COCOAnnotationVisualizer:
                 coco_category = self.parser.categories.get_category_from_id(id=coco_annotation.category_id)
                 cat_name_list.append(coco_category.name)
                 kpt_skeleton_list.append(coco_category.skeleton)
+                kpt_label_list_list.append(coco_category.keypoints)
             self.save(
                 img=img,
                 bbox_list=bbox_list,
                 kpt_list_list=kpt_list_list,
                 kpt_skeleton_list=kpt_skeleton_list,
+                kpt_label_list_list=kpt_label_list_list,
                 seg_list=seg_list,
                 cat_name_list=cat_name_list,
                 file_name=coco_image.file_name
